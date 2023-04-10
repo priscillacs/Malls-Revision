55d362bbfe92fec8462a1b2a771164c18fd50125
Priscilla Celine Setiawan <120435531+priscilla-celine@users.noreply.github.com>
2023-04-10 20:54:21 +0800

    update theme

--- contexts/ThemeProvider.js
+++ contexts/ThemeProvider.js
@@ -1,46 +1,45 @@
-import React, {createContext, useContext, useState, useEffect} from "react";
-import {View, StyleSheet} from 'react-native';
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
 import { darkTheme, defaultTheme } from "../config/theme/theme";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ThemeContext = createContext();
 
-const ThemeProvider = ({children}) =>{
-        const [theme, setTheme] = useState(defaultTheme);
-        const [isLoadingTheme, setIsLoadingTheme] = useState(true);
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(defaultTheme);
+  const [isLoadingTheme, setIsLoadingTheme] = useState(true);
 
-        const findOldTheme = async () => {
-            const themeMode = await AsyncStorage.getItem('themeMode');
-            
-            if (themeMode !== null){
-                themeMode === 'default'? setTheme(defaultTheme): setTheme(darkTheme)
-                setIsLoadingTheme(false);
-            }
-            setIsLoadingTheme(false);
-        };
+  const findOldTheme = async () => {
+    const themeMode = await AsyncStorage.getItem("themeMode");
 
-        useEffect(()=>{
-            findOldTheme();
-        }, []);
+    if (themeMode !== null) {
+      themeMode === "default" ? setTheme(defaultTheme) : setTheme(darkTheme);
+      setIsLoadingTheme(false);
+    }
+    setIsLoadingTheme(false);
+  };
 
-        const updateTheme = currentThemeMode =>{
-            const newTheme = currentThemeMode === 'default' ? darkTheme : defaultTheme;
-            setTheme(newTheme)
-            AsyncStorage.setItem('themeMode', newTheme.themeMode)
-        }
+  useEffect(() => {
+    findOldTheme();
+  }, []);
 
-    return(
-        <ThemeContext.Provider value = {{theme, isLoadingTheme, updateTheme}}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  const updateTheme = (currentThemeMode) => {
+    const newTheme = currentThemeMode === "default" ? darkTheme : defaultTheme;
+    setTheme(newTheme);
+    AsyncStorage.setItem("themeMode", newTheme.themeMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, isLoadingTheme, updateTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export const useTheme = () => useContext(ThemeContext);
 
-
 const styles = StyleSheet.create({
-    container:{}
-})
+  container: {},
+});
 
 export default ThemeProvider;
